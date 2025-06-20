b0d324df06602f13ada5955d5a02ab3a693a7a21
Riham Chbouki <chbouki.riham@gmail.com>
2025-06-20 19:32:21 +0200

    transformation: it works
    Signed-off-by: Riham Chbouki <chbouki.riham@gmail.com>

--- transformation.py
+++ transformation.py
@@ -3,6 +3,7 @@ import os
 import sys
 import numpy as np
 from matplotlib import pyplot as plt
+from matplotlib.backend_bases import KeyEvent
 from plantcv import plantcv as pcv
 import cv2
 import warnings
@@ -11,39 +12,96 @@ import math
 warnings.filterwarnings("ignore", category=RuntimeWarning)
 _original_acos = math.acos
 
+
 def safe_acos(x):
-    x = max(min(x, 1.0), -1.0)  # clamp to valid domain
+    x = max(min(x, 1.0), -1.0)
     return _original_acos(x)
 
 
 math.acos = safe_acos
 
 
+def close_on_key(event: KeyEvent) -> None:
+    """
+    Close the window when the ESC key is pressed
+    :param event: keyboard event
+    :return:
+    """
+    if event.key == 'escape':
+        plt.close(event.canvas.figure)
+
+
 def mask_transformation(labeled_mask: np.ndarray, img: np.ndarray):
+    """
+    Apply a mask transformation to an image
+    :param labeled_mask: generated mask from image
+    :param img: original image
+    :return: Mask transformed image
+    """
     return pcv.apply_mask(img=img, mask=labeled_mask, mask_color="white")
 
 
 def gaussian_blur_transformation(gray: np.ndarray) -> np.ndarray:
+    """
+    Apply a gaussian blur to an image
+    :param gray: Gray generated image from original image
+    :return: Gaussian blur transformed image
+    """
     return pcv.gaussian_blur(img=gray, ksize=(5, 5))
 
 
 def object_analysis_transformation(labeled_mask: np.ndarray, img: np.ndarray):
+    """
+    Apply an object analysis to an image
+    :param labeled_mask: generated mask from image
+    :param img: original image
+    :return: object analysis transformed image
+    """
     return pcv.analyze.size(img=img, labeled_mask=labeled_mask, n_labels=1)
 
 
-def watershed_segmentation_transformation(labeled_mask: np.ndarray, img: np.ndarray):
-    watershed = pcv.watershed_segmentation(rgb_img=img, mask=labeled_mask, distance=15, label="default")
+def watershed_segmentation_transformation(mask: np.ndarray, img: np.ndarray):
+    """
+    Apply a watershed segmentation transformation to an image
+    :param mask: generated mask from image
+    :param img: original image
+    :return: Watershed segmentation transformed image
+    """
+    watershed = pcv.watershed_segmentation(
+        rgb_img=img,
+        mask=mask,
+        distance=15,
+        label="default"
+    )
     return pcv.visualize.colorize_label_img(label_img=watershed)
 
 
 def pseudo_landmarks_transformation(labeled_mask: np.ndarray, img: np.ndarray):
+    """
+    Apply a pseudo landmarks transformation to an image
+    :param labeled_mask: generated mask from image
+    :param img: original image
+    :return: Landmarks transformed image
+    """
     return pcv.homology.acute(img=img, mask=labeled_mask, win=25, threshold=90)
 
+
 def color_histogram_transformation(labeled_mask: np.ndarray, img: np.ndarray):
-        return pcv.visualize.histogram(img=img, mask=labeled_mask, hist_data=True)
+    """
+    Apply a color histogram transformation to an image
+    :param labeled_mask: generated mask from image
+    :param img: original image
+    :return: Color Histogram on original image
+    """
+    return pcv.visualize.histogram(img=img, mask=labeled_mask, hist_data=True)
 
 
-def plot_transformations(transformed_images):
+def plot_transformations(transformed_images: dict):
+    """
+    Plot the 5 image transformations
+    :param transformed_images: dictionary of transformed images
+    :return:
+    """
     img = transformed_images.get("Original_Image")
 
     images = [
@@ -65,6 +123,7 @@ def plot_transformations(transformed_images):
     cmaps = [None, 'gray', None, None, None, None]
 
     fig, ax = plt.subplots(3, 2, figsize=(10, 6))
+    fig.canvas.mpl_connect('key_press_event', close_on_key)
     ax = ax.flatten()
 
     for i, (image, title, cmap) in enumerate(zip(images, titles, cmaps)):
@@ -82,11 +141,23 @@ def plot_transformations(transformed_images):
 
 
 def plot_histogram(hist_data, save_path):
-    plt.figure(figsize=(10, 6))
+    """
+    Plot the color histogram of an image
+    :param hist_data: histogram plantCV data
+    :param save_path: path where to save the histogram
+    :return:
+    """
+    fig = plt.figure(figsize=(10, 6))
+    fig.canvas.mpl_connect('key_press_event', close_on_key)
 
     for color in ['blue', 'green', 'red']:
         subset = hist_data[hist_data['color channel'] == color]
-        plt.plot(subset['pixel intensity'], subset['hist_count'], color=color, label=f'{color} channel')
+        plt.plot(
+            subset['pixel intensity'],
+            subset['hist_count'],
+            color=color,
+            label=f'{color} channel'
+        )
 
     plt.xlabel('Pixel Intensity')
     plt.ylabel('Count')
@@ -102,10 +173,19 @@ def plot_histogram(hist_data, save_path):
 
 
 def image_transformation(img_path: str) -> dict:
+    """
+    Apply 6 types of image transformations
+    :param img_path: path of original image
+    :return: dictionary of transformed images
+    """
     # Load image and convert it to gray scale
     img, path, _ = pcv.readimage(img_path)
     gray_img = pcv.rgb2gray_lab(rgb_img=img, channel='l')
-    binary_img = pcv.threshold.binary(gray_img, threshold=120, object_type='dark')
+    binary_img = pcv.threshold.binary(
+        gray_img,
+        threshold=120,
+        object_type='dark'
+    )
 
     # Apply Gaussian blur transformation
     blurred = gaussian_blur_transformation(gray_img)
@@ -165,25 +245,57 @@ def argparse_flags() -> argparse.Namespace:
     Parse the command-line arguments.
     :return: parsed commandline arguments
     """
-    parser = argparse.ArgumentParser(description="Display 6 types of Image Transformation")
-    parser.add_argument('-src', '--source', type=str, required=True, help="Input file or directory")
-    parser.add_argument('-dst', '--destination', type=str, help="Output folder (optional if only one image)")
+    parser = argparse.ArgumentParser(
+        description="Display 6 types of Image Transformation"
+    )
+    parser.add_argument(
+        '-src',
+        '--source',
+        type=str,
+        required=True,
+        help="Input file Image or directory"
+    )
+    parser.add_argument(
+        '-dst',
+        '--destination',
+        type=str,
+        help="Output folder (optional if only one image)"
+    )
 
     return parser.parse_args()
 
 
 def save_image_with_landmarks(transformed_images, save_path):
+    """
+    Save original image with pseudo-landmarks
+    :param transformed_images: dictionary of transformed images
+    :param save_path: path where to save the image
+    :return:
+    """
     img = transformed_images.get("Original_Image")
     landmarks = transformed_images.get("Pseudo_Landmarks")
 
     landmarks = np.array(landmarks)  # Ensure it's a NumPy array
     for (x, y) in landmarks.reshape(-1, 2):
-        cv2.circle(img, (int(x), int(y)), radius=3, color=(0, 255, 0), thickness=-1)
+        cv2.circle(
+            img,
+            (int(x), int(y)),
+            radius=3,
+            color=(0, 255, 0),
+            thickness=-1
+        )
 
     cv2.imwrite(save_path, img)
 
 
 def save_transformed_images(transformed_images, output_dir, img_path):
+    """
+    Save the transformed images in output_dir
+    :param transformed_images: dictionary with transformed images
+    :param output_dir:
+    :param img_path:
+    :return:
+    """
     for type, element in transformed_images.items():
         base_name = os.path.splitext(os.path.basename(img_path))[0]
         new_filename = f"{base_name}_{type}.JPG"
@@ -203,7 +315,7 @@ def save_transformed_images(transformed_images, output_dir, img_path):
                 path = os.path.join(output_dir, f"{new_filename}")
                 cv2.imwrite(path, bgr)
 
-            print(f"Saving {type} transformation for {img_path}")
+        print(f"Saving {type} transformation for {img_path}")
 
 
 if __name__ == "__main__":
@@ -225,16 +337,22 @@ if __name__ == "__main__":
         transformed_images = image_transformation(images_path[0])
 
         if args.destination:
-            save_transformed_images(transformed_images, output_dir, images_path[0])
+            save_transformed_images(
+                transformed_images,
+                output_dir,
+                images_path[0]
+            )
         else:
             # Plot the 6 Image Transformations
             plot_transformations(transformed_images)
-            plot_histogram(hist_data = transformed_images.get("Histogram"), save_path=None)
+            plot_histogram(
+                hist_data=transformed_images.get("Histogram"),
+                save_path=None
+            )
     else:
         if not output_dir:
             print("Error: You must specify a destination folder.")
             sys.exit(1)
         for img_path in images_path:
-            print(f"Processing {img_path}")
             transformed_images = image_transformation(img_path)
             save_transformed_images(transformed_images, output_dir, img_path)
