8f9f42940c96f99c898c0cd1fafff44708adece9
Riham Chbouki <chbouki.riham@gmail.com>
2025-06-23 16:47:17 +0200

    train: train and visualize training accuracy
    Signed-off-by: Riham Chbouki <chbouki.riham@gmail.com>

--- train.py
+++ train.py
@@ -82,4 +82,61 @@ if __name__ == "__main__":
 
     train_ds, val_ds = standardize_and_configure_data(train_ds, val_ds)
 
-    
+    # Create the model
+    print("Creating the model...")
+    num_classes = len(class_names)
+
+    model = Sequential([
+        keras.Input(shape=(img_height, img_width, 3)),
+        layers.Rescaling(1. / 255),
+        layers.Conv2D(16, 3, padding='same', activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Conv2D(32, 3, padding='same', activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Conv2D(64, 3, padding='same', activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Flatten(),
+        layers.Dense(128, activation='relu'),
+        layers.Dense(num_classes)
+    ])
+
+    model.compile(optimizer='adam',
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  metrics=['accuracy'])
+
+    model.summary()
+
+    # Training the model
+    print("Training the model...")
+    epochs = 10
+    history = model.fit(
+        train_ds,
+        validation_data=val_ds,
+        epochs=epochs
+    )
+
+    model.save("leaffliction_model.keras")
+
+    # Visualize training results
+    print("Visualizing model training accuracy...")
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs_range = range(epochs)
+
+    plt.figure(figsize=(8, 8))
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs_range, acc, label='Training Accuracy')
+    plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+    plt.legend(loc='lower right')
+    plt.title('Training and Validation Accuracy')
+
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs_range, loss, label='Training Loss')
+    plt.plot(epochs_range, val_loss, label='Validation Loss')
+    plt.legend(loc='upper right')
+    plt.title('Training and Validation Loss')
+    plt.show()
