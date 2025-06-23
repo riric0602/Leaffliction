66b7260f4cec688bd7054c86eba511dc060de8b4
Riham Chbouki <chbouki.riham@gmail.com>
2025-06-23 15:55:11 +0200

    train: standardize and configure data
    Signed-off-by: Riham Chbouki <chbouki.riham@gmail.com>

--- train.py
+++ train.py
@@ -1,6 +1,10 @@
 import matplotlib.pyplot as plt
 import tensorflow as tf
 import pathlib
+import numpy as np
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
 
 
 batch_size = 32
@@ -36,6 +40,22 @@ def set_training_validation_dataset(data_dir: pathlib.Path):
     return train_ds, val_ds
 
 
+def standardize_and_configure_data(train_ds, val_ds):
+    # Standardize data to be in [0, 1] range instead of [0, 255]
+    normalization_layer = layers.Rescaling(1. / 255)
+    normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+
+    # Configure dataset for better performance
+    train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+    val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+    image_batch, labels_batch = next(iter(normalized_ds))
+    first_image = image_batch[0]
+    print(f"Range of data: [{np.min(first_image)}, {np.max(first_image)}]")
+
+    return train_ds, val_ds
+
+
 if __name__ == "__main__":
     # Load leaves dataset
     data_dir = pathlib.Path("./leaves/images")
@@ -48,7 +68,7 @@ if __name__ == "__main__":
     class_names = train_ds.class_names
     print(f"Training Class Names: {class_names}")
 
-    # Visualize data from the training set
+    # Visualize some data from the training set
     plt.figure(figsize=(10, 10))
     for images, labels in train_ds.take(1):
         for i in range(9):
@@ -56,10 +76,10 @@ if __name__ == "__main__":
             plt.imshow(images[i].numpy().astype("uint8"))
             plt.title(class_names[labels[i]])
             plt.axis("off")
-    plt.title('Random 9 images from the training set')
+    plt.suptitle("Random 9 Images from the Training Set", fontsize=20)
+    plt.tight_layout(rect=[0, 0, 1, 0.95])  # Leave space for the big title
     plt.show()
 
-    # Configure dataset for better performance
-    train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
-    val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+    train_ds, val_ds = standardize_and_configure_data(train_ds, val_ds)
+
     
